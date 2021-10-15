4b4801483bfdf56e6d8ef471edbab4d35fd2fc25
Ashley Baker <bakerashley56@gmail.com>
2021-10-15 12:53:31 -0700

    wk1 algo 2 done

--- Algos/wk1.js
+++ Algos/wk1.js
@@ -1,9 +1,9 @@
 /* 
-  Given an array of ints, find all the non-consecutive integers
-  A number is non-consecutive if it is NOT exactly 1 larger than the previous element.
-  The first element is never considered non-consecutive.
-  Return an array of objects where each object contains the number itself
-  and it's index.
+    Given an array of ints, find all the non-consecutive integers
+    A number is non-consecutive if it is NOT exactly 1 larger than the previous element.
+    The first element is never considered non-consecutive.
+    Return an array of objects where each object contains the number itself
+    and it's index.
 */
 
 const nums1 = [1, 2, 3, 4, 6, 7, 8, 10];
@@ -52,3 +52,55 @@ console.log(allNonConsecutive(nums1));
 console.log(allNonConsecutive(nums2));
 console.log(allNonConsecutive(nums3));
 console.log(allNonConsecutive(nums4));
+
+/* 
+    Given an array of integers
+    return the index where the smallest integer is located
+    return -1 if there is no smallest integer (array is empty) 
+    since -1 is not a valid index, this indicates it couldn't be found
+    If the smallest integer occurs more than once, return the index of the first one.
+*/
+
+const nums1 = [5, 2, 3];
+const expected1 = 1;
+
+const nums2 = [5, 4, 2, 2, 3];
+const expected2 = 2;
+
+const nums3 = [];
+const expected3 = -1;
+
+/**
+ * Returns the index of the smallest number from the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {number} Index of smallest number or -1 if empty given array.
+ */
+function indexOfMinVal(nums) {
+    if(!nums[0]) {
+        return -1;
+    }
+
+    let min = nums[0];
+    let minIdx = 0;
+
+    for(i=1;i<nums.length;i++) {
+        if(nums[i]<min) {
+            min = nums[i];
+            minIdx = i;
+        }
+    }
+
+    return minIdx;
+}
+
+// Tests
+const result1 = indexOfMinVal(nums1);
+console.log(result1, "should be", expected1);
+
+const result2 = indexOfMinVal(nums2);
+console.log(result2, "should be", expected2);
+
+const result3 = indexOfMinVal(nums3);
+console.log(result3, "should be", expected3);
